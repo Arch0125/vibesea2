3a2a6cb59875945d6b9ab8ed3529462736f693b8
Arch0125 <dasarchisman25@gmail.com>
2022-06-06 03:13:49 +0530

    Add Connect metamask functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { MetaMaskProvider } from './api/components/context/metamsk.context'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
+    <MetaMaskProvider>
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider> 
+    </MetaMaskProvider>
   )
 }
 
--- pages/api/components/Navbar.tsx
+++ pages/api/components/Navbar.tsx
@@ -1,16 +1,17 @@
 import { Flex } from '@chakra-ui/react';
 import * as React from 'react';
 import { Button, ButtonGroup } from '@chakra-ui/react'
+import NotConnectedModal from './NotConnectedModal';
 
 const Navbar=() =>{
     return(
-        <Flex px={"20px"} py={"10px"} flexDirection={"row"} width={"100vw"} bgColor={"black"} >
+        <Flex px={"20px"} py={"15px"} flexDirection={"row"} width={"100vw"} bgColor={"black"} >
             <ButtonGroup>
                 <Button variant={"ghost"} colorScheme={"whiteAlpha"}>Home</Button>
                 <Button variant={"ghost"} colorScheme={"whiteAlpha"}>Explore</Button>
                 <Button variant={"ghost"} colorScheme={"whiteAlpha"}>Create</Button>
             </ButtonGroup>
-            <Button ml={"70%"} >Connect wallet</Button>
+            <NotConnectedModal/>
         </Flex>
     )
 }
