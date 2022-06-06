e03b992a6f21a4e4b93df38d4a0dea4a522ce0b5
Arch0125 <dasarchisman25@gmail.com>
2022-06-06 15:36:26 +0530

    Fix Explore Page

--- pages/ExplorePage.tsx
+++ pages/ExplorePage.tsx
@@ -1,31 +1,56 @@
-import { Box, Divider, Flex, Text } from '@chakra-ui/react';
-import * as React from 'react';
-import { useMetamask } from './api/components/context/metamsk.context';
-import Navbar from './api/components/Navbar';
-import PostList from './api/components/PostList';
+import { Box, Divider, Flex, Text } from "@chakra-ui/react";
+import * as React from "react";
+import { useMetamask } from "./api/components/context/metamsk.context";
+import Navbar from "./api/components/Navbar";
+import PostList from "./api/components/PostList";
 
-const ExplorePage =() =>{
+const ExplorePage = () => {
+  const { provider, walletAddress, balance } = useMetamask();
 
-    const{provider,walletAddress, balance}=useMetamask();
+  return (
+    <>
+      <Navbar />
+      <Flex
+        flexDirection={"row"}
+        bgColor={"black"}
+        height={"100vh"}
+        width={"100vw"}
+      >
+        <Box textAlign={"start"} width={"40%"} height={"100vh"}>
+          <Box
+            ml={"20px"}
+            mt={"30px"}
+            width={"80%"}
+            height={"fit-content"}
+            padding={"20px"}
+            bgColor={"#171717"}
+            borderRadius={"20px"}
+            borderColor={"gray"}
+            borderWidth={"1.5px"}
+          >
+            <Text color={"white"} fontSize={"xl"} fontWeight={"bold"}>
+              Your Profile
+            </Text>
+            <Text mb={"10px"} color={"white"} mt={"10px"}>
+              {walletAddress}
+            </Text>
+            <Divider />
+            <Text
+              color={"white"}
+              mt={"10px"}
+              fontWeight={"semibold"}
+              fontSize={"20px"}
+            >
+              Balance : &nbsp; {balance}
+            </Text>
+          </Box>
+        </Box>
+        <Box>
+          <PostList />
+        </Box>
+      </Flex>
+    </>
+  );
+};
 
-    return(
-        <>
-        <Navbar/>
-        <Flex flexDirection={"row"} bgColor={"black"} height={"100vh"} width={"100vw"} >
-            <Box textAlign={"start"} width={"40%"} height={"100vh"} >
-                <Box ml={"20px"} mt={"30px"} width={"80%"} height={"fit-content"} padding={"20px"} bgColor={"#171717"} borderRadius={"20px"} borderColor={"gray"} borderWidth={"1.5px"} >
-                    <Text color={"white"} fontSize={"xl"} fontWeight={"bold"} >Your Profile</Text>
-                    <Text mb={"10px"} color={"white"} mt={"10px"} >{walletAddress}</Text>
-                    <Divider/>
-                    <Text color={"white"} mt={'10px'} fontWeight={'semibold'} fontSize={"20px"} >Balance : &nbsp; {balance}</Text>
-                </Box>
-            </Box>
-            <Box>
-                <PostList/>
-            </Box>
-        </Flex>
-        </>
-    )
-}
-
-export default ExplorePage
+export default ExplorePage;
